426e1f806c7817fe01718d37a2c8b9996e538bc9
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-10-04 14:00:16 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,9 @@
+class Sum {
+    public static void main(String[] args) {
+        int c = 0;
+        for(int i=0;i<=100;i++){
+            c = c+i;
+        }
+        System.out.println(c);
+    }
+}
